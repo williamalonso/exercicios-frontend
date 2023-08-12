4bf075aad3fcf6af451bc3f5415e0e241db976e7
William Alonso <williamalonso@outlook.com>
2023-08-12 18:41:24 +0000

    updating megamenu

--- 07_megaMenu/index.js
+++ 07_megaMenu/index.js
@@ -2,16 +2,28 @@
 var largura = window.screen.width; // pega largura da tela
 
 // Script para exibir/ocultar barrinha azul do menu web aberto
-if( largura >= 991) { // se tela for maior que 991px
+function exibeBarrinha() {
+	if( largura >= 991) { // se tela for maior que 991px
 
-    document.getElementsByClassName("dropdown-toggle")[0].addEventListener("click", function(){
-        document.getElementsByClassName("hr-infra")[0].classList.toggle("display-none");
-    });
-    
-    // quando fechar dropdown (clicar em qualquer lugar da tela), removemos a barrinha
-    window.onclick = (e) => {
-        if(!document.getElementsByClassName("dropdown-menu")[0].classList.contains("show")) {
-            document.getElementsByClassName("hr-infra")[0].classList.add("display-none");
-        }
-  }
+		const dropdownLink = document.querySelector('.dropdown-toggle');
+
+	    dropdownLink.addEventListener('click', () => {
+	        if (dropdownLink.classList.contains('show')) {
+	            console.log('A classe "show" está presente.');
+	            document.getElementsByClassName("hr-infra")[0].classList.remove("display-none");
+	        } else {
+	            console.log('A classe "show" não está presente.');
+	            document.getElementsByClassName("hr-infra")[0].classList.add("display-none");
+	        }
+	    });
+	    
+	    // quando fechar dropdown (clicar em qualquer lugar da tela), removemos a barrinha
+	    window.onclick = (e) => {
+	        if(!document.getElementsByClassName("dropdown-menu")[0].classList.contains("show")) {
+	            document.getElementsByClassName("hr-infra")[0].classList.add("display-none");
+	        }
+	  }
+	}
 }
+window.addEventListener('load', exibeBarrinha, false);
+window.addEventListener('resize', exibeBarrinha, false);
