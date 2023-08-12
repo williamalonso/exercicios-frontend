82e83e6b1ed8d6fe3a69271368f3d6293c5318a2
William Alonso <williamalonso@outlook.com>
2023-08-12 20:06:50 +0000

    updating megamenu

--- 07_megaMenu/index.js
+++ 07_megaMenu/index.js
@@ -1,4 +1,3 @@
-
 var largura = window.screen.width; // pega largura da tela
 
 // Script para exibir/ocultar barrinha azul do menu web aberto
@@ -25,3 +24,19 @@ function exibeBarrinha() {
 }
 window.addEventListener('load', exibeBarrinha, false);
 window.addEventListener('resize', exibeBarrinha, false);
+
+var navbar = document.getElementById('container__navbar');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY >= 420) {
+			navbar.classList.add('container__navbar--scroll');
+			navbar.classList.remove('container__navbar--scroll-out');
+	} else if(window.scrollY < 420 && window.scrollY >= 1)  {
+		navbar.classList.remove('container__navbar--scroll');
+		navbar.classList.add('container__navbar--scroll-out');
+	}
+	else {
+			navbar.classList.remove('container__navbar--scroll');
+			navbar.classList.remove('container__navbar--scroll-out');
+	}
+});
