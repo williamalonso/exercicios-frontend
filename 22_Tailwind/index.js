332402a242b03e0305e6774097054434433a8484
William Alonso <williamalonso@outlook.com>
2023-09-08 19:33:45 -0300

    tailwind exercise

--- 22_Tailwind/index.js
+++ 22_Tailwind/index.js
@@ -6,17 +6,18 @@ tailwind.config = {
     extend: {
       fontFamily: {
         oswald: 'Oswald, ui-serif',
-      }
-    },
-    colors: {
-      'clifford': '#da373d',
+      },
+      colors: {
+        'clifford': '#da373d',
+      },
     }
-  }
+  },
+  plugins: [],
 }
 
 // Element
 var elemento = `
-  <div class="container mx-auto flex justify-center items-center h-96" style="background-color: blue">
+  <div class="absolute container left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center h-96 bg-sky-600">
     <h1 class="text-3xl font-bold underline text-clifford font-oswald">
       Hello world!
     </h1>
