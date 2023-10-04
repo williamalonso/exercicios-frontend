09ff5a3045bb4baff4ccb4bb8011d859f798cac7
William Alonso <williamalonso@outlook.com>
2023-10-03 22:40:38 -0300

    wireframe loader

---
+++ 24_wireframeLoader/index.js
@@ -0,0 +1,14 @@
+// Tailwind config
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: 'Oswald, ui-serif',
+      },
+      colors: {
+        'clifford': '#da373d',
+      },
+    }
+  },
+  plugins: [],
+}
