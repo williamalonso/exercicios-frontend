e0f38d8f62d4509a45c64d1f53891b0ee7129ff6
William Alonso <williamalonso@outlook.com>
2023-12-28 22:35:56 -0300

    new custom slider

---
+++ 33_customSlider2/index.js
@@ -0,0 +1,34 @@
+const initSlider = () => {
+  
+  const cardList = document.querySelector(".tab-header .slider-item-container");
+  const slideButtons = document.querySelectorAll(".tab-header .slide-button-arrow");
+  const cardItem = document.querySelectorAll(".slider-item-container .slider-item");
+
+  slideButtons.forEach( button => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = cardList.clientWidth * direction;
+      cardList.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
+      })
+    })
+  })
+
+  cardItem.forEach( item => {
+    item.addEventListener("click", () => {
+      const rect = item.getBoundingClientRect();
+      const cardMidpoint = rect.left + rect.width / 2;
+      const containerMidpoint = cardList.getBoundingClientRect().left + cardList.offsetWidth / 2;
+      const direction = cardMidpoint < containerMidpoint ? -1 : 1;
+      const scrollAmount = (item.clientWidth + 50) * direction;
+      cardList.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth"
+      });
+    })
+  })
+
+}
+
+window.addEventListener("load", initSlider);
