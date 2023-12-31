7737e285910fffb9875b6ab4840d0306eec4f551
William Alonso <williamalonso@outlook.com>
2023-12-31 18:32:48 -0300

    adjusting form slider

--- 33_customSlider2/index.js
+++ 33_customSlider2/index.js
@@ -1,14 +1,8 @@
 const getFormWidth  = () => {
-  // Obtém o elemento com a classe "slider-form-container"
+  
   const sliderFormContainer = document.querySelector('.slider-form-container');
-
-  // Obtém o valor da propriedade grid-template-columns
   const gridColumnValue = window.getComputedStyle(sliderFormContainer).getPropertyValue('grid-template-columns');
-
-  // Divide a string para obter uma matriz de valores
   const columnValues = gridColumnValue.split(' ');
-
-  // Obtém o valor da primeira coluna
   const firstColumnValue = parseInt(columnValues[0], 10);
 
   return firstColumnValue;
@@ -20,8 +14,6 @@ const slideForm = (item) => {
   const formList = document.querySelector(".left-content .slider-form-container");
   const index = parseInt(itemId.split('-').pop(), 10);
   const formWidth = getFormWidth();
-
-  // Calcula a posição do item em relação ao início do .slider-form-container
   const itemPosition = (index - 1) * formWidth;
 
   formList.scrollTo({
